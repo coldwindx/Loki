45e5724845047844135abaf50fd079c92f2d07ea
zhulin.007 <zhulin.007@bytedance.com>
2025-12-08 17:39:37 +0800

    t

--- loki-netty/src/test/java/com/coldwindx/loki/flux/FluxTest.java
+++ loki-netty/src/test/java/com/coldwindx/loki/flux/FluxTest.java
@@ -3,8 +3,13 @@ package com.coldwindx.loki.flux;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 public class FluxTest {
@@ -23,5 +28,22 @@ public class FluxTest {
                 .doOnError(e->log.error(e.getMessage()));
         flux.subscribe();
         Thread.sleep(1000000);
+    }
+    
+    @Test
+    public void test2() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Sinks.Many<String> sink = Sinks.many().replay().all();
+        Flux<String> flux = sink.asFlux().doOnTerminate(latch::countDown);
+
+        for(int i = 0; i < 10; ++i){
+            sink.tryEmitNext("val:" + i);
+        }
+        sink.tryEmitComplete();
+        System.out.println("sink.tryEmitComplete");
+
+        flux.subscribe(System.out::println);
+        latch.await();
+
     }
 }
