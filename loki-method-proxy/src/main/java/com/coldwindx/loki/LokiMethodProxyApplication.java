98f2fa152023de7f5ba857a064bb0424c5c34ca0
zhulin.007 <zhulin.007@bytedance.com>
2025-12-28 21:22:19 +0800

    创建项目

---
+++ loki-method-proxy/src/main/java/com/coldwindx/loki/LokiMethodProxyApplication.java
@@ -0,0 +1,13 @@
+package com.coldwindx.loki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokiMethodProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LokiMethodProxyApplication.class, args);
+    }
+
+}
